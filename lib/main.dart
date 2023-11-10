ff86350b86a1a65514fbd11ff4c50db3300be639
Naman Manchanda <108816021+Naman278@users.noreply.github.com>
2023-11-10 20:18:01 +0530

    fixed firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/Responses/mobile_screen_layout.dart';
 import 'package:instagram_flutter/Responses/responsive_screen_layout.dart';
 import 'package:instagram_flutter/Responses/web_screen_Layout.dart';
 import 'package:instagram_flutter/config/colors.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
